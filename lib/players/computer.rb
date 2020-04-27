883b2d6874c6ecd75b2f57578d4089c5fd88a39a
Micah McCallum <micahmccallum@outlook.com>
2020-04-27 22:41:21 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,7 +4,6 @@ module Players
     def move(board)
       @board = board
       get_input(@board)
-
     end
 
     def get_input(board)
@@ -16,8 +15,12 @@ module Players
         @board.taken?("5") ? int = Board::CORNERS.sample : int = Board::CENTER
       elsif @board.turn_count == 2
         int = two_in_a_row
-      elsif @board.turn_count == 3
+      elsif can_win?
+        int = win
+      elsif can_block?
         int = block
+      else
+        int = [Board::CORNERS.sample, Board::EDGES.sample].sample
       end
       input = index_to_input(int) if int
     end
@@ -25,33 +28,59 @@ module Players
     def two_in_a_row
       saved_combo = []
       temp_saved_combo = []
-      position = nil
       Game::WIN_COMBINATIONS.each do |combo|
-        combo.each do |position|
-          temp_saved_combo << combo.reject {|int| int == position}  if @board.cells[position] == token
+        temp_cells = []
+        combo.each do |cell|
+          temp_cells << cell if @board.cells[cell] == self.token
         end
+        temp_saved_combo << combo if temp_cells.count > 0
       end
-      opponent_moves = @board.cells.find_all {|cell| cell == opponent_token}
+      opponent_moves = []
+      @board.cells.each_with_index do |cell, index|
+        opponent_moves << index if cell == opponent_token
+      end
       temp_saved_combo.each do |combo|
-        saved_combo << combo unless combo.include?(opponent_moves)
+        saved_combo << combo unless combo.include?(opponent_moves.first)
       end
-      (saved_combo.sample).sample
+      (saved_combo.sample).reject {|cell| @board.cells[cell] == self.token}.sample
+    end
+
+    def can_block?
+      opponent_moves = []
+      @board.cells.each_with_index do |cell, index|
+        opponent_moves << index if cell == opponent_token
+      end
+      Game::WIN_COMBINATIONS.each do |combo|
+        position_count = 0
+        combo.each do |position|
+          position_count += 1 if opponent_moves.include?(position)
+
+        end
+        @block_combo = combo if position_count > 1 && combo.include?(" ")
+
+      end
+      @block_combo ? true : false
     end
 
     def block
-      temp_saved_combo = []
-      saved_combo = []
-      Game::WIN_COMBINATIONS.each do |combo|
-        combo.each do |position|
-          temp_saved_combo << combo if @board.cells[position == opponent_token]
-        end
-      end
-      temp_saved_combo.each do |combo|
-        combo.each do |postion|
-          saved_combo << combo if
-        end
-      end
+      input = @block_combo.reject {|cell| @board.cells[cell] == opponent_token}.sample
 
+    end
+
+    def can_win?
+      token_count = 0
+      Game::WIN_COMBINATIONS.each do |combo|
+        token_count = 0
+        combo.each do |position|
+          token_count += 1 if @board.cells[position] == token
+        end
+        @win_combo = combo if token_count > 1 && combo.include?(" ")
+      end
+      @win_combo ? true : false
+    end
+
+    def win
+      @win_combo.reject {|position| @board.cells[position] == token }.sample
     end
 
     def index_to_input(int)
@@ -59,7 +88,7 @@ module Players
     end
 
     def opponent_token
-      TOKENS.reject{|toquen| toquen == token}
+      TOKENS.reject{|toquen| toquen == token}.sample
     end
 
 
