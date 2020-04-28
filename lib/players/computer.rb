a21c110eac9ec2d0d97a5e7c228d23345ab7d43a
Micah McCallum <micahmccallum@outlook.com>
2020-04-28 03:25:33 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,84 +3,85 @@ module Players
 
     def move(board)
       @board = board
-      get_input(@board)
+      @opponent_token = self.opponent_token
+      get_input
     end
 
-    def get_input(board)
-      @board = board
+    def get_input
       @input = nil
       if @board.turn_count == 0
         int = [Board::CORNERS.sample, Board::CENTER].sample
       elsif @board.turn_count == 1
         @board.taken?("5") ? int = Board::CORNERS.sample : int = Board::CENTER
-      elsif @board.turn_count == 2
-        int = two_in_a_row
       elsif can_win?
         int = win
       elsif can_block?
         int = block
+      elsif can_play_two_in_a_row?
+        int = two_in_a_row
       else
         int = [Board::CORNERS.sample, Board::EDGES.sample].sample
       end
       input = index_to_input(int) if int
     end
 
-    def two_in_a_row
-      saved_combo = []
-      temp_saved_combo = []
+    def can_play_two_in_a_row?
       Game::WIN_COMBINATIONS.each do |combo|
-        temp_cells = []
+        token_count = 0
+        opponent_token_count = 0
         combo.each do |cell|
-          temp_cells << cell if @board.cells[cell] == self.token
+          combo_cell = @board.cells[cell]
+          if combo_cell == token
+            token_count += 1
+          elsif combo_cell == @opponent_token
+            opponent_token_count += 1
+          end
         end
-        temp_saved_combo << combo if temp_cells.count > 0
+        @two_in_a_row_combo = combo if token_count > 0 && opponent_token_count == 0
+         end
+      @two_in_a_row_combo ? true : false
+    end
+
+    def can_win?
+      @win_combo = []
+      Game::WIN_COMBINATIONS.each do |combo|
+        token_count = 0
+        tokens = []
+        combo.each do |position|
+          token_count += 1 if @board.cells[position] == token
+          tokens << @board.cells[position]
+        end
+        @win_combo = combo if token_count == 2 && tokens.include?(" ")
+
       end
-      opponent_moves = []
-      @board.cells.each_with_index do |cell, index|
-        opponent_moves << index if cell == opponent_token
-      end
-      temp_saved_combo.each do |combo|
-        saved_combo << combo unless combo.include?(opponent_moves.first)
-      end
-      (saved_combo.sample).reject {|cell| @board.cells[cell] == self.token}.sample
+      @win_combo.count > 1 ? true : false
     end
 
     def can_block?
-      opponent_moves = []
-      @board.cells.each_with_index do |cell, index|
-        opponent_moves << index if cell == opponent_token
-      end
+      @block_combo = false
       Game::WIN_COMBINATIONS.each do |combo|
-        position_count = 0
+        opponent_moves = []
+        blank_cells = []
         combo.each do |position|
-          position_count += 1 if opponent_moves.include?(position)
-
+          opponent_moves << position if @board.cells[position] == @opponent_token
+          blank_cells << position if @board.cells[position] == " "
         end
-        @block_combo = combo if position_count > 1 && combo.include?(" ")
-
+        @block_combo = combo if opponent_moves.count == 2 && blank_cells.count == 1
       end
       @block_combo ? true : false
     end
 
-    def block
-      input = @block_combo.reject {|cell| @board.cells[cell] == opponent_token}.sample
-
-    end
-
-    def can_win?
-      token_count = 0
-      Game::WIN_COMBINATIONS.each do |combo|
-        token_count = 0
-        combo.each do |position|
-          token_count += 1 if @board.cells[position] == token
-        end
-        @win_combo = combo if token_count > 1 && combo.include?(" ")
-      end
-      @win_combo ? true : false
+    def two_in_a_row
+      @two_in_a_row_combo.reject {|cell| @board.cells[cell] == self.token}.sample
     end
 
     def win
-      @win_combo.reject {|position| @board.cells[position] == token }.sample
+      binding.pry
+      @win_combo.sample.reject {|position| @board.cells[position] == self.token }.sample
+    end
+
+    def block
+      @block_combo.find {|cell| @board.cells[cell] == " "}
     end
 
     def index_to_input(int)
@@ -88,9 +89,8 @@ module Players
     end
 
     def opponent_token
-      TOKENS.reject{|toquen| toquen == token}.sample
+      Player::TOKENS.find {|toquen| toquen != token}
     end
-
 
   end
 end
