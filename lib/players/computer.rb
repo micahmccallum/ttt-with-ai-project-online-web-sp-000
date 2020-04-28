14e5bd29f6c87808e390f86ca91d07a54f96201c
Micah McCallum <micahmccallum@outlook.com>
2020-04-28 20:18:50 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,6 +9,7 @@ module Players
 
     def get_input
       @input = nil
+      int = nil
       if @board.turn_count == 0
         int = [Board::CORNERS.sample, Board::CENTER].sample
       elsif @board.turn_count == 1
@@ -18,7 +19,7 @@ module Players
       elsif can_block?
         int = block
       elsif can_play_two_in_a_row?
-        int = two_in_a_row
+        int =  two_in_a_row
       else
         int = [Board::CORNERS.sample, Board::EDGES.sample].sample
       end
@@ -26,48 +27,42 @@ module Players
     end
 
     def can_play_two_in_a_row?
+      @two_in_a_row_combo = false
       Game::WIN_COMBINATIONS.each do |combo|
-        token_count = 0
-        opponent_token_count = 0
-        combo.each do |cell|
-          combo_cell = @board.cells[cell]
-          if combo_cell == token
-            token_count += 1
-          elsif combo_cell == @opponent_token
-            opponent_token_count += 1
-          end
+        temp_array = []
+        combo.each do |position|
+          temp_array << @board.cells[position]
         end
-        @two_in_a_row_combo = combo if token_count > 0 && opponent_token_count == 0
-         end
-      @two_in_a_row_combo ? true : false
+        temp_array.sort!
+        @two_in_a_row_combo = combo if temp_array == [" ", " ", "#{token}"]
+      end
+      @two_in_a_row_combo
     end
 
     def can_win?
       @win_combo = false
       Game::WIN_COMBINATIONS.each do |combo|
-        token_count = []
-        tokens = []
+        temp_array = []
         combo.each do |position|
-          token_count += 1 if @board.cells[position] == token
-          tokens << @board.cells[position]
+          temp_array << @board.cells[position]
         end
-        @win_combo = combo if token_count == 2 && tokens.include?(token)
+        temp_array.sort!
+        @win_combo = combo if temp_array == [" ", "#{token}", "#{token}"]
       end
-      @win_combo > 1 ? true : false
+      @win_combo
     end
 
     def can_block?
       @block_combo = false
       Game::WIN_COMBINATIONS.each do |combo|
-        opponent_moves = []
-        blank_cells = []
+        temp_array = []
         combo.each do |position|
-          opponent_moves << position if @board.cells[position] == @opponent_token
-          blank_cells << position if @board.cells[position] == " "
+          temp_array << @board.cells[position]
         end
-        @block_combo = combo if opponent_moves.count == 2 && blank_cells.count == 1
+        temp_array.sort!
+        @block_combo = combo if temp_array == [" ", "#{@opponent_token}", "#{@opponent_token}"]
       end
-      @block_combo ? true : false
+      @block_combo
     end
 
     def two_in_a_row
@@ -75,7 +70,6 @@ module Players
     end
 
     def win
-      binding.pry
       @win_combo.sample.reject {|position| @board.cells[position] == self.token }.sample
     end
 
